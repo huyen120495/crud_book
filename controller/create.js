467578975e59a314f780781094a722b4c7277c45
huyen120495 <huyen120495@gmail.com>
2018-03-07 16:09:27 +0700

    edit edit.js

--- controller/create.js
+++ controller/create.js
@@ -1,5 +1,5 @@
 module.exports = function(request, response) {
-    request.app.get('book_provider').make(request.body).then(book => {
+    request.app.get('book_factory_from_db').make(request.body).then(book => {
         request.app.get('book_repository').save(book).then(() => {
             response.status(201).send({message : 'created'});
         });
