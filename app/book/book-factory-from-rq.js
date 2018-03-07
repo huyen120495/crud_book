ab5632fb01fa0a1ff5897f227d4fb52e28f2a7e9
huyen120495 <huyen120495@gmail.com>
2018-03-07 16:33:51 +0700

    edit check

--- app/book/book-factory-from-rq.js
+++ app/book/book-factory-from-rq.js
@@ -1,5 +1,4 @@
 const Book       = require('./book');
-const Publisher  = require('../publisher/publisher');
 const Connection = require('../../database/connection');
 
 class BookFactoryFromRQ{
@@ -8,8 +7,8 @@ class BookFactoryFromRQ{
      * 
      * @param {Connection} connection 
      */
-    constructor(publisher_factory_from_db) {
-        this.publisher_factory_from_db = publisher_factory_from_db;
+    constructor(publisher_provider) {
+        this.publisher_provider = publisher_provider;
     }
 
     /**
@@ -21,23 +20,11 @@ class BookFactoryFromRQ{
         let book = new Book(bookRaw.title, bookRaw.author);
         book.setId(bookRaw.id);
         book.setPrice(bookRaw.price);
-        return this.publisher_factory_from_db.make(bookRaw.publisher_id)
+        return this.publisher_provider.make(bookRaw.publisher_id)
         .then(publisher => {
             book.setPublisher(publisher);
             return book;
         });
-
-        // return this.connection('publishers').select().where({id : bookRaw.publisher_id}).limit(1)
-        // .then(publishers => {
-        //     if (publishers[0]) {
-        //         let publisher = new Publisher(publishers[0].name);
-        //         publisher.setId(publishers[0].id);
-        //         publisher.setAddress(publishers[0].address);
-        //         publisher.setPhone(publishers[0].phonenumber);
-        //         book.setPublisher(publisher);
-        //     } 
-        //     return book;
-        // });
 
     }
 }
--- middleware/check-author-null.js
+++ middleware/check-author-null.js
@@ -1,6 +1,4 @@
 module.exports = function(request, response, next) {
-    if (!request.body.author) {
-	return response.status(400).send({message : "author must not null"});
-    }
+    if (!request.body.author) return response.status(400).send({message : "author must not null"});
     next();
 }
